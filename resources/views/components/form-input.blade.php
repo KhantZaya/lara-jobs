e77e9b62438fb66ade4bb8cbed551ac002341599
Khant Zaya <khantzaya003@gmail.com>
2025-12-07 12:07:31 +0630

    Refactor: Form Components

---
+++ resources/views/components/form-input.blade.php
@@ -0,0 +1,4 @@
+<div class="flex items-center rounded-md bg-gray-100 pl-3 focus-within:ring-2 focus-within:ring-indigo-500">
+    <input
+        {{ $attributes->merge(['class' => 'block w-full bg-transparent py-2 pr-3 pl-1 text-base text-gray-900 placeholder-gray-400 focus:outline-none']) }} />
+</div>
--- resources/views/job/create.blade.php
+++ resources/views/job/create.blade.php
@@ -14,45 +14,31 @@
                 <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <!-- Job Title -->
                     <div class="sm:col-span-4">
-                        <label for="job-title" class="block text-sm font-medium text-gray-700">Job Title</label>
-                        <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-gray-100 pl-3 focus-within:ring-2 focus-within:ring-indigo-500">
-                                <input id="job-title" name="job_title" type="text" placeholder="Product Designer"
-                                    class="block w-full bg-transparent py-2 pr-3 pl-1 text-base text-gray-900 placeholder-gray-400 focus:outline-none" />
-                            </div>
-                        </div>
-                        @error('job_title')
-                            <p class="text-red-500 text-sm">{{ $message }}</p>
-                        @enderror
+                        <x-form-label for="job-title">Job Title</x-form-label>
+                        <x-form-input id="job-title" name="job_title" type="text" placeholder="Product Designer" />
+
+                        <x-form-error name="job_title" />
                     </div>
 
                     <!-- Job Salary -->
                     <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm font-medium text-gray-700">Salary</label>
-                        <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-gray-100 pl-3 focus-within:ring-2 focus-within:ring-indigo-500">
-                                <input id="salary" name="salary" type="text" placeholder="180,000 Per Year"
-                                    class="block w-full bg-transparent py-2 pr-3 pl-1 text-base text-gray-900 placeholder-gray-400 focus:outline-none" />
-                            </div>
-                        </div>
-                        @error('salary')
-                            <p class="text-red-500 text-sm">{{ $message }}</p>
-                        @enderror
+                        <x-form-label for="salary">Salary</x-form-label>
+                        <x-form-input id="salary" name="salary" type="text" placeholder="180,000 Per Year" />
+
+                        <x-form-error name="salary" />
+
                     </div>
 
                     <!-- Description -->
                     <div class="col-span-full">
-                        <label for="job_description" class="block text-sm font-medium text-gray-700">Description</label>
+                        <x-form-label for="job-description">Description</x-form-label>
                         <div class="mt-2">
-                            <textarea id="job_description" name="job_description" rows="3"
+                            <textarea id="job-description" name="job_description" rows="3"
                                 class="h-48 block w-full rounded-md bg-gray-100 px-3 py-2 text-base text-gray-900 placeholder-gray-400 focus:ring-2 focus:ring-indigo-500 focus:outline-none sm:text-sm"
                                 placeholder="Job Description"></textarea>
                         </div>
-                        @error('job_description')
-                            <p class="text-red-500 text-sm">{{ $message }}</p>
-                        @enderror
+                        <x-form-error name="job_description" />
+
                     </div>
                 </div>
             </div>
